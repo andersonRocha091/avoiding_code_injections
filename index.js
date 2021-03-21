a2cec2d6ca420a8808166feb96282b701d81306b
Anderson <andersonecomp091@gmail.com>
2021-03-20 23:52:23 -0300

    Reunning command ls -la at SO via child_process

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import http from "http";
+import { exec } from "child_process";
 
 const port = process.env.PORT || 3000;
 
-async function runCommandWithExec(command){
-  return `Hello world`;
+async function runCommandWithExec(command) {
+  const promise = new Promise((resolve, reject) => {
+    exec(command, (err, res) => (err ? reject(err) : resolve(res)));
+  });
+  const response = (await promise);
+  return response;
 }
 
 http.createServer(async (req,res)=>{
@@ -13,11 +18,11 @@ http.createServer(async (req,res)=>{
     exec: runCommandWithExec
   }
   console.log('URL: ', path);
-  for await(const data of req){
-    const command = JSON.parse(data);
+  for await (const data of req) {
+    const { command } = JSON.parse(data);
     const response = await routes[path](command);
-    console.log('response ', response);
-    res.write(response);
+    console.log("response ", response);
+    res.write(JSON.stringify(response));
     res.end();
   }
 }).listen(port, () => {
