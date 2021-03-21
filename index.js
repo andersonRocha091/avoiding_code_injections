bbc6f4ad5ddd3ca479090c5e9ada0a13b4d892bc
Anderson <andersonecomp091@gmail.com>
2021-03-21 00:22:26 -0300

    spawn still not preventing user from access any other folder

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import http from "http";
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
+// exec allows multiple commands to be running
+//which is not a good practice
 
 const port = process.env.PORT || 3000;
+
+//runs commands in a individual way
+//better control of what can be ran, and where
+async function runCommandWithSpawn(command) {
+  //separating commands from arguments
+  //splititing by spaces removing empty lines
+
+  //running straight at documments, so avoiding user
+  //getting into paths not allowed
+  const [cmd, ...args] = command.split(/\s/);
+  console.log('argument: ', cmd, args);
+  const {stdout, stderr} = spawn(cmd, args || [],{cwd: './documents'});
+  for await(const result of stdout){
+
+    return result.toString().split('\n').filter(i=>!!i);
+  }
+}
 
 async function runCommandWithExec(command) {
   const promise = new Promise((resolve, reject) => {
@@ -16,7 +35,8 @@ http.createServer(async (req,res)=>{
   //Removing everything which is not a word
   const path = req.url.replace(/\W/,'');
   const routes = {
-    exec: runCommandWithExec
+    exec: runCommandWithExec,
+    spawn: runCommandWithSpawn
   }
   console.log('URL: ', path);
   for await (const data of req) {
--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
+# exec
 curl -X POST http://localhost:3000/exec --data '{"command":"ls -la"}'
 curl -X POST http://localhost:3000/exec --data '{"command":"cd documents && ls -la"}' | jq
 curl -X POST http://localhost:3000/exec --data '{"command":"cd documents && echo aeee > newfile.txt"}'| jq
 curl -X POST http://localhost:3000/exec --data '{"command":"cd documents && cat newfile.txt"}'| jq
+curl -X POST http://localhost:3000/exec --data '{"command":"ls -la ~"}'| jq
+
+#spawn
+curl -X POST http://localhost:3000/spawn --data '{"command":"cd documents && ls -la"}' | jq
+curl -X POST http://localhost:3000/spawn --data '{"command":"ls -la ~"}' | jq
